440c3b904593959a16b48f0a9caf7d4741fba04d
Eric Campbell <ericcampbell59@gmail.com>
2023-06-07 08:43:28 -0500

    create run script

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,11 +6,17 @@ FILENAME=stack.json
 if [ -f "${FILENAME}" ]; then
     if [ -s "${FILENAME}" ]; then
         CONTENTS=`cat ${FILENAME} | sed -e 's/^[[:space:]]*//'`
-        if [[ "$CONTENTS" == "null" ]]; then
+        if [[ "$CONTENTS" != "null" ]]; then
+            echo "${FILENAME} exists and not empty"
+            
+            echo "Installing dependencies"
+            npm install
+            echo "Deploying CDK"
+
+            STACK_ID=$STACK_ID FIRST_DEPLOY=$FIRST_DEPLOY npx cdk deploy
+        else 
             echo "${FILENAME} is null"
             exit 1
-        else 
-            echo "${FILENAME} exists and not empty"
         fi
     else
         echo "${FILENAME} exists but empty"
